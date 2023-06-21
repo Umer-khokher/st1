6b4b89d63b900d45610e317ce98c3a7f4ec9fd65
Senesto <umerkhokar@plandstudios.com>
2023-06-21 17:12:54 +0500

    msg1

--- src/components/covid.js
+++ src/components/covid.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const Covid = () => {
-  return <div>Covid</div>;
+const CovidData = async () => {
+  try {
+    const res = await fetch("https://api.covid19india.org/data.json");
+    console.log(res);
+  } catch (err) {
+    console.log(err);
+  }
 };
+useEffect(() => {
+  getCovidData();
+}, []);
+return (
+  <>
+    <h1>LIVE</h1>
+    <h2>COVID-19 CORONAVIRUS TRACKER</h2>
+  </>
+);
 
 export default Covid;
